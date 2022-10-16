edea2b1a815746c3d929abc53da11e88ad8753ae
Sir Code <taylorschneider07@yahoo.com>
2022-10-16 05:44:33 -0400

    fix timing save error + add delays

--- src/main/java/net/scmods/blockentities/RandomRedstoneBlockEntity.java
+++ src/main/java/net/scmods/blockentities/RandomRedstoneBlockEntity.java
@@ -8,23 +8,34 @@ import net.scmods.RandomRedstone;
 
 public class RandomRedstoneBlockEntity extends BlockEntity implements BlockEntityHost.Hosted {
     private int power = 0;
+    private int delay = 0;
+    private int ticks = 0;
     private boolean latched = false;
 
     public RandomRedstoneBlockEntity(BlockPos pos, BlockState state) {
         super(RandomRedstone.RRB_BlockEntity, pos, state);
     }
 
+    public void set(int value) {
+        delay = value;
+    }
+
     @Override
     public void tick()
     {
-        this.latched = false;
-        this.world.updateNeighbors(getPos(), getCachedState().getBlock());
+        if (ticks >= delay * 2) {
+            this.latched = false;
+            assert this.world != null;
+            this.world.updateNeighbors(getPos(), getCachedState().getBlock());
+            ticks = 0;
+        } else ticks++;
     }
 
     public int getWeakRedstonePower()
     {
         if (!this.latched)
         {
+            assert this.world != null;
             this.power = this.world.random.nextBetween(0, 15);
             this.latched = true;
         }
--- src/main/java/net/scmods/blocks/RandomRedstoneBlock.java
+++ src/main/java/net/scmods/blocks/RandomRedstoneBlock.java
@@ -3,17 +3,32 @@ package net.scmods.blocks;
 import net.minecraft.block.*;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.block.entity.BlockEntityType;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.network.message.MessageType;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.state.StateManager;
+import net.minecraft.state.property.IntProperty;
+import net.minecraft.state.property.Properties;
+import net.minecraft.state.property.Property;
+import net.minecraft.text.Text;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.world.BlockView;
+import net.minecraft.world.World;
 import net.scmods.BlockEntityHost;
 import net.scmods.RandomRedstone;
 import net.scmods.blockentities.RandomRedstoneBlockEntity;
 
 public class RandomRedstoneBlock extends Block implements BlockEntityHost {
+    public static final IntProperty DELAY = Properties.DELAY;
     public RandomRedstoneBlock(AbstractBlock.Settings settings) {
         super(settings);
+        this.setDefaultState(this.stateManager.getDefaultState().with(DELAY, 1));
     }
+
     @Override
     public BlockRenderType getRenderType(BlockState state) {
         return BlockRenderType.MODEL;
@@ -25,8 +40,24 @@ public class RandomRedstoneBlock extends Block implements BlockEntityHost {
     }
 
     @Override
-    public BlockEntity createBlockEntity(BlockPos blockPos, BlockState blockState) {
-        return new RandomRedstoneBlockEntity(blockPos, blockState);
+    public BlockEntity createBlockEntity(BlockPos pos, BlockState state) {
+        RandomRedstoneBlockEntity ent = new RandomRedstoneBlockEntity(pos, state);
+        ent.set(state.get(DELAY));
+        return ent;
+    }
+
+    public ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit) {
+        if (!player.getAbilities().allowModifyWorld) {
+            return ActionResult.PASS;
+        } else {
+            BlockEntity bety = world.getBlockEntity(pos);
+            if (bety instanceof RandomRedstoneBlockEntity) {
+                world.setBlockState(pos, state.cycle(DELAY), 3);
+                ((RandomRedstoneBlockEntity)bety).set(state.get(DELAY));
+            }
+            player.sendMessage(Text.of("Changed delay to ".concat(String.valueOf((state.get(DELAY) * 2))).concat(" ticks")), true);
+            return ActionResult.success(world.isClient);
+        }
     }
 
     @Override
@@ -39,5 +70,9 @@ public class RandomRedstoneBlock extends Block implements BlockEntityHost {
         if (world.getBlockEntity(pos) instanceof RandomRedstoneBlockEntity rng)
             return rng.getWeakRedstonePower();
         return 0;
+    }
+
+    protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
+        builder.add(DELAY);
     }
 }
